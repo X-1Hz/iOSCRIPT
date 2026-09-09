fb5b8fecbf70f5641faa0ded0ebcc7c326a1bc2c
1Hz <cr497979@gmail.com>
2026-09-09 16:17:43 +0700

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,4 +1,4 @@
 getgenv().Games = {
     [102222157152089] = "https://raw.githubusercontent.com/X-1Hz/iOSCRIPT/refs/heads/main/Crack%20a%20Rock.lua",
-    [88891488197895] = "https://raw.githubusercontent.com/X-1Hz/iOSCRIPT/refs/heads/main/Crack%20a%20Rock.lua",
+    [88891488197895] = "https://raw.githubusercontent.com/X-1Hz/iOSCRIPT/refs/heads/main/Grow%20a%20Beehive.lua",
 }
